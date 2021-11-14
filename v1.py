2c015466b00713703c4b33c07e2d1290d1184e7e
Sanket <82602204+Xerofishe@users.noreply.github.com>
2021-11-14 19:26:54 +0530

    Update v1.py

--- v1.py
+++ v1.py
@@ -8,7 +8,7 @@ pygame.display.set_caption('Game')
 
 running = True
 while running:
-    screen.fill((0, 255, 0))
+    screen.fill((0, 255, 255))
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
